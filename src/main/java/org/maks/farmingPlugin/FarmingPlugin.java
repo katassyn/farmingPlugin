08c07b5f8f4983b5f63bbdcb30b9d793357f9621
katassyn <mastu2115@gmail.com>
2025-08-24 15:38:23 +0200

    1st

---
+++ src/main/java/org/maks/farmingPlugin/FarmingPlugin.java
@@ -0,0 +1,17 @@
+package org.maks.farmingPlugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class FarmingPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
